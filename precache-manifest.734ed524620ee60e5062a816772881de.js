c206d14bc242ab786c90228506238d37ebf41a43
darkster663 <emilp988@gmail.com>
2020-10-26 12:57:40 +0200

    Updates

---
+++ precache-manifest.734ed524620ee60e5062a816772881de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c578b16bb72733fe9d12af5e81727039",
+    "url": "/money-tracker/index.html"
+  },
+  {
+    "revision": "e858f4da71a6023cd049",
+    "url": "/money-tracker/static/js/2.3d4af7ae.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/money-tracker/static/js/2.3d4af7ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97c12fb692bccb32b5db",
+    "url": "/money-tracker/static/js/main.f7c4fdce.chunk.js"
+  },
+  {
+    "revision": "acd2850899a0b3061707",
+    "url": "/money-tracker/static/js/runtime-main.37284696.js"
+  },
+  {
+    "revision": "2fc04467584007b1a004fdf625f11588",
+    "url": "/money-tracker/static/media/nutrition_supplements.2fc04467.png"
+  }
+]);
